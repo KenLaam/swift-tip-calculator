09037968d2e04018d5e22f16608d26bb58209ea9
Ken Lam <hoangvu23108@gmail.com>
2016-10-01 18:32:59 +0700

    add locale currency symbol

--- Tip Calulator/ViewController.swift
+++ Tip Calulator/ViewController.swift
@@ -18,12 +18,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-     
     }
     
     override func viewWillAppear(_ animated: Bool) {
         txtBill.becomeFirstResponder()
-        
+        calculateTip(NSNull.self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,15 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func getLocaleCurrency(){
+        
+    }
+    
     @IBAction func tapMainView(_ sender: AnyObject) {
         view.endEditing(true)
     }
     @IBAction func calculateTip(_ sender: AnyObject) {
+        let currency = Locale.current.currencySymbol!
         let percent = [0.1, 0.15, 0.2]
         let bill = (Double) (txtBill.text!) ?? 0
         
-        lblTip.text = String(format: "$ %.2f", bill * percent[percentList.selectedSegmentIndex])
-        lblTotal.text = String(format: "$ %.2f", bill * (1+percent[percentList.selectedSegmentIndex]))
+        lblTip.text = String(format: "%@ %.2f", currency, bill * percent[percentList.selectedSegmentIndex])
+        lblTotal.text = String(format: "%@ %.2f", currency, bill * (1+percent[percentList.selectedSegmentIndex]))
     }
 
 }
